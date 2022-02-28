b7deb6390286f22d664595228a41235a3ccf2b0f
fanxb <fleyx20@outlook.com>
2022-02-27 23:15:07 -0900

    deploy:修改部署设置

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# 配置统计key
-export VUE_APP_QIEZI_HOST="https://fleyx.com";
-export VUE_APP_QIEZI_KEY="aab3cad381f54ca5b7b9abeb2e09320a";
+# 替换为自己的部署地址
+export VUE_APP_QIEZI_HOST="http://localhost:8080";
+# 替换为自己申请的key
+export VUE_APP_QIEZI_KEY="d862c12a68ad4d579c6066ac2f064a07";
 
 base=$(cd "$(dirname "$0")";pwd)
-echo $VUE_APP_QIEZI_HOST
-echo $VUE_APP_QIEZI_KEY
 #
 # 前端打包
 docker run -it --rm --user ${UID} -e VUE_APP_QIEZI_HOST=${VUE_APP_QIEZI_HOST} -e VUE_APP_QIEZI_KEY=${VUE_APP_QIEZI_KEY}   -v $base/qiezi_front:/opt/front node:lts-buster-slim  bash -c "cd /opt/front &&   yarn --registry https://registry.npm.taobao.org && yarn build"
