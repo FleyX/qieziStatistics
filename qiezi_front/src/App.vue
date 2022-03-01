b2276d041dc3993b3eaad5af7e12d75a0e938df6
fanxb <fleyx20@outlook.com>
2022-03-01 23:50:06 +0800

    fix:修复vue页面不展示uv，pv统计

--- qiezi_front/src/App.vue
+++ qiezi_front/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <router-view />
 
-  <div  style="">
-    <div id="qieziStatisticHtmlHostPv" style="display: none; width: 50%; text-align: right">页面访问次数:<span id="qieziStatisticHtmlHostPvValue"></span>次&nbsp;</div>
-    <div id="qieziStatisticHtmlHostUv" style="display: none; width: 50%; text-align: left">&nbsp;总访客数:<span id="qieziStatisticHtmlHostUvValue"></span>人</div>
-  </div>
 </template>
 
 <script setup>
