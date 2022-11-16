797268b13a11a3e4b7ac5084c48c16eb8d65e897
fanxb <fleyx20@outlook.com>
2022-11-16 17:04:53 +0800

    feat

---
+++ qiezi_front/src/views/manage/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>应用管理</div>
+</template>
+
+<script setup>
+import { onMounted, ref, reactive } from "vue";
+import { get, post } from "../util/HttpUtil";
+</script>
+
+<style lang="less" scoped>
+.title {
+  font-size: 2em;
+  font-weight: 600;
+}
+.captchaItem {
+  display: flex;
+}
+</style>
