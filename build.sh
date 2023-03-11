a6522b536c7e682a26b175c785541c751e5d9867
fanxb <fanxb.tl@gmail.com>
2023-03-11 10:05:12 -0500

    deploy:change

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export VUE_APP_QIEZI_KEY="d862c12a68ad4d579c6066ac2f064a07";
 base=$(cd "$(dirname "$0")";pwd)
 #
 # 前端打包
-docker run -it --rm --user ${UID} -e VUE_APP_QIEZI_HOST=${VUE_APP_QIEZI_HOST} -e VUE_APP_QIEZI_KEY=${VUE_APP_QIEZI_KEY}   -v $base/qiezi_front:/opt/front node:lts-slim  bash -c "cd /opt/front &&   yarn --registry https://registry.npm.taobao.org && yarn build"
+docker run  --rm --user ${UID} -e VUE_APP_QIEZI_HOST=${VUE_APP_QIEZI_HOST} -e VUE_APP_QIEZI_KEY=${VUE_APP_QIEZI_KEY}   -v $base/qiezi_front:/opt/front node:lts-slim  bash -c "cd /opt/front &&   yarn --registry https://registry.npm.taobao.org && yarn build"
 # 后端打包
-docker run -it --rm --user ${UID} -v $base/data/maven/mavenRep:/var/maven/.m2: -v $base/data/maven/settings.xml:/usr/share/maven/conf/settings.xml -v $base/qieziBackend:/code maven:3-openjdk-11-slim  bash -c "cd /code && mvn clean install"
+docker run  --rm --user ${UID} -v $base/data/maven/mavenRep:/var/maven/.m2: -v $base/data/maven/settings.xml:/usr/share/maven/conf/settings.xml -v $base/qieziBackend:/code maven:3-openjdk-11-slim  bash -c "cd /code && mvn clean install"
 
